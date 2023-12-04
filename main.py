61883359f0569f7939eb6ec18b833d042d34064b
Misha <70710273+MbNextT1me@users.noreply.github.com>
2023-12-04 13:35:42 +0800

    Add files via upload

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 import tkinter as tk
 from tkinter import ttk
+import colorsys
 
 SIZE_disk = (600, 600, 3)
 CENTER = (SIZE_disk[0] // 2, SIZE_disk[1] // 2)
@@ -13,15 +14,26 @@ def get_colors():
     colors = [(255, 0, 0), (242, 255, 0), (26, 255, 0), (0, 255, 221), (0, 106, 255), (140, 0, 255), (255, 0, 242)]
     return colors
 
+
+def generate_color_table(num_colors):
+    colors = []
+    for i in range(num_colors):
+        hue = int(i * (360 / num_colors)) % 360
+        saturation = 0.8
+        value = 0.8
+        rgb = tuple(int(c * 255) for c in colorsys.hsv_to_rgb(hue / 360, saturation, value))
+        colors.append(rgb)
+
+    return colors
+
 def get_alphabet():
     alphabet = {}
-    colors = get_colors()
-    q_colors = len(colors)
+    color_table = generate_color_table(33)
 
-    color_counter = 0
     for i in range(1040, 1072):
-        alphabet[chr(i)] = ((colors[q_colors - color_counter // q_colors - 1]), (colors[color_counter % q_colors]))
-        color_counter += 1
+        color1 = color_table[(i - 1040) % 33]
+        color2 = color_table[(i - 1040 + 1) % 33]
+        alphabet[chr(i)] = (color1, color2)
 
     alphabet[" "] = ((255, 255, 255), (255, 255, 255))
     return alphabet
@@ -31,13 +43,13 @@ def draw_code(disk, msg: str, alphabet):
     angle_message = 2 * DELTA * len(msg)
 
     while angle < 360:
-        disk = cv.ellipse(disk, CENTER, (RADIUS, RADIUS), 0, angle, angle + DELTA, alphabet[msg[angle % angle_message // (2*DELTA)]][0], -1)
+        disk = cv.ellipse(disk, CENTER, (RADIUS, RADIUS), 0, angle, angle + DELTA, alphabet[msg[angle % angle_message // (2*DELTA)]][0][::-1], -1)
         angle += DELTA
-
-        disk = cv.ellipse(disk, CENTER, (RADIUS, RADIUS), 0, angle, angle + DELTA, alphabet[msg[(angle - 8) % angle_message // (2*DELTA)]][1], -1)
+        disk = cv.ellipse(disk, CENTER, (RADIUS, RADIUS), 0, angle, angle + DELTA, alphabet[msg[(angle - 8) % angle_message // (2*DELTA)]][1][::-1], -1)
         angle += DELTA
 
     return disk
+
 
 def create_color_table(alphabet):
     table_size = (len(alphabet), 3, 3)
@@ -48,21 +60,19 @@ def create_color_table(alphabet):
         table_image[i, 0, :] = 255
         table_image[i, 1, :] = colors[0]
         table_image[i, 2, :] = colors[1]
-
     return table_image
 
-def plot_color_table(table_image, alphabet):
-    _, ax = plt.subplots(figsize=(8, 10))
+def plot_color_table(legend_image, alphabet):
+    _, ax = plt.subplots(figsize=(10, 6))
+
+    cell_text = []
+    for letter, colors in alphabet.items():
+        cell_text.append([letter, colors[0], colors[1]])
+    table = ax.table(cellText=cell_text, cellColours=legend_image / 255.0, loc='center', cellLoc='center', colLabels=None)
+    table.auto_set_font_size(False)
+    table.set_fontsize(8)
+
     ax.axis('off')
-
-    table = ax.table(cellText=None, cellColours=table_image / 255.0, cellLoc='center', loc='center')
-
-    for i, (letter, _) in enumerate(alphabet.items()):
-        table[(i, 0)].get_text().set_text(letter)
-
-    table[(len(alphabet) - 1, 0)].get_text().set_text("Пробел")
-
-    plt.title("Алфавит")
     plt.show()
 
 def on_alphabet_button_click():
@@ -83,10 +93,9 @@ def on_encode_button_click():
 
 root = tk.Tk()
 root.title("Кодирование диска")
-root.geometry("400x200")  # Задаем размеры окна
+root.geometry("400x200")
 
 alphabet = get_alphabet()
-
 alphabet_button = ttk.Button(root, text="Вывести алфавит", command=on_alphabet_button_click)
 alphabet_button.pack(pady=10)
 
